fb4577acff6f961fb7fe7bd45628ec94e00e0d8f
sajidahw <sajidahwahdy@Sajidahs-MacBook-Air.local>
2024-03-18 22:54:09 -0700

    updated css

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -92,7 +92,7 @@ const Chat = () => {
         display: "flex",
         flex: 1,
         width: "100%",
-        height: "auto", //100%
+        height: "100%", //100%
         mt: 3,
         gap: 3,
       }}
